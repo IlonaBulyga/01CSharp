98d5c50bf2acc92851095fe5f9d8fab762a3313f
Ilona Bulyga <ilona.bulyga@gmail.com>
2024-02-01 11:44:05 -0500

    Lesson2:\Task4

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -2,6 +2,7 @@
 int [] array = {2, 4, 7, 3, 9};
 int i = 0;
 int max = array[0];
+
 while (i < n)
 {
     if (array[i] > max)
@@ -9,5 +10,25 @@ while (i < n)
         max = array [i]; 
     }
     i = i +1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array [j]; 
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
 }
 Console.WriteLine(max);
